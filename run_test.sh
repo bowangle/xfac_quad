66306a43ce55444d794e87024be316fc8425f34c
Bauge Angel <baugeangel@gmail.com>
2026-07-30 09:40:19 +0200

    Update cmake to not propagate test of deeper level

--- run_test.sh
+++ run_test.sh
@@ -7,24 +7,30 @@ if [ ! -d "$BUILD_DIR" ]; then
     exit 1
 fi
 
-cd "$BUILD_DIR"
+cd "$BUILD_DIR" || exit 1
+
+PREFIX=xfac_quad_test_
 
 echo "==> Running xfac_quad tests..."
 
-for tgt in \
-    Test_SVDDecomp_double Test_SVDDecomp_float128 Test_SVDDecomp_dd128 \
-    Test_matrix_ci_double Test_matrix_ci_float128 Test_matrix_ci_dd128 \
-    Test_tensor_ci_double Test_tensor_ci_float128 Test_tensor_ci_dd128
+for name in \
+SVDDecomp_double SVDDecomp_float128 SVDDecomp_dd128 \
+matrix_ci_double matrix_ci_float128 matrix_ci_dd128 \
+tensor_ci_double tensor_ci_float128 tensor_ci_dd128
 do
+    tgt="$PREFIX$name"
     echo "--- $tgt ---"
     ./"$tgt" || exit 1
 done
 
 if [ "$1" = "-slow" ]; then
     echo "==> Running benchmarks..."
-    ./Test_benchmark_double "[.]" || exit 1
-    ./Test_benchmark_float128 "[.]" || exit 1
-    ./Test_benchmark_dd128 "[.]" || exit 1
+    for name in benchmark_problem_double benchmark_problem_float128 benchmark_problem_dd128
+    do
+        tgt="$PREFIX$name"
+        echo "--- $tgt ---"
+        ./"$tgt" "[.]" || exit 1
+    done
 fi
 
 echo "==> All tests passed."
